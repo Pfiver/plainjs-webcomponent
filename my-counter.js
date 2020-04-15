52c14b16a1306b37e373877882bddaa5b9b05189
Glitch (pfiver-plainjs-webcomponent) <none>
2020-04-15 12:41:02 +0000

    🎊🐜 Checkpoint ./my-counter.js:5867248/2148

--- my-counter.js
+++ my-counter.js
@@ -1,53 +1,54 @@
 class MyCounter extends HTMLElement {
   
   count = 0;
+
+  static html = `
+    <button id="dec" class="large btn">-</button>
+    <span id="val"></span>
+    <button id="inc" class="large btn">+</button>
+  `;
+
+  static css = `
+    * {
+      font-size: 200%;
+    }
+
+    span {
+      width: 4rem;
+      display: inline-block;
+      text-align: center;
+    }
+
+    button {
+      width: 4rem;
+      height: 4rem;
+      border: none;
+      border-radius: 10px;
+      background-color: seagreen;
+      color: white;
+    }
+  `;
   
   constructor() {
-    
+
     super();
-    this.count = 0;
-
-    const style = `
-      * {
-        font-size: 200%;
-      }
-
-      span {
-        width: 4rem;
-        display: inline-block;
-        text-align: center;
-      }
-
-      button {
-        width: 4rem;
-        height: 4rem;
-        border: none;
-        border-radius: 10px;
-        background-color: seagreen;
-        color: white;
-      }
-    `;
-
-    const html = `
-    <button id="dec" class="large btn">-</button>
-    <span class="large value">${this.count}</span>
-    <button id="inc" class="large btn">+</button>
-    `;
 
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
-    <style>
-      ${style}
-    </style>
-    ${html}
+      <style>
+        ${MyCounter.css}
+      </style>
+      ${MyCounter.html}
     `;
 
     this.buttonInc = this.shadowRoot.getElementById("inc");
     this.buttonDec = this.shadowRoot.getElementById("dec");
-    this.spanValue = this.shadowRoot.querySelector("span");
+    this.spanValue = this.shadowRoot.getElementById("val");
 
     this.inc = this.inc.bind(this);
     this.dec = this.dec.bind(this);
+    
+    this.update();
   }
 
   inc() {
